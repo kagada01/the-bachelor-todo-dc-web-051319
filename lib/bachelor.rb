33ad9323391b39b2295f4ed6bff5f3b9962aa05b
Dan Kagan <dankagan7@gmail.com>
2019-05-02 14:23:21 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -48,7 +48,7 @@ def get_average_age_for_season(data, season)
   data.each do |season, contestants|
     contestants.each do |contestant|
       total_age = age + contestant["age"].to_i
-      
+      num_contestants += 1
     binding.pry
     end
   end
